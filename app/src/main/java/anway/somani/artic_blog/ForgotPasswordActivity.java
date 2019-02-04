d31e5b31d7035462374051768a6d729313f3a580
anwaysomani <anwaysomani@gmail.com>
2019-02-04 08:13:47 +0530

    Extracted project fiels from folder

---
+++ app/src/main/java/anway/somani/artic_blog/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package anway.somani.artic_blog;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password);
+    }
+}
---
+++ app/src/main/java/anway/somani/artic_blog/MainActivity.java
@@ -0,0 +1,25 @@
+package anway.somani.artic_blog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+}
